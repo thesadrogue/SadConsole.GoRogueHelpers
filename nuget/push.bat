345a46ef0e245f5b923d5ed7c621ac7444bfa422
Chris3606 <cridley106@gmail.com>
2020-03-30 19:49:58 -0500

    Fixed nuget deployment automation scripts.

--- nuget/push.bat
+++ nuget/push.bat
@@ -1,2 +1,2 @@
-nuget push SadConsole.GoRogue.%1-debug.nupkg -Source https://api.nuget.org/v3/index.json
-nuget push SadConsole.GoRogue.%1.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push SadConsole.GoRogueHelpers.%1-debug.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push SadConsole.GoRogueHelpers.%1.nupkg -Source https://api.nuget.org/v3/index.json
